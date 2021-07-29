1bbfe5f9ee9cedd0846cd32e874cb9145de233f4
Sergey Lyulchenko <solo300m@yandex.ru>
2021-07-29 12:29:36 +0400

    controller Logger

--- main/java/com/example/testgit/controller/webPageController.java
+++ main/java/com/example/testgit/controller/webPageController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.logging.Logger;
+
 @Controller
 public class webPageController {
 
     @GetMapping("/")
     public String startPage(Model model){
         model.addAttribute("title", "Главная страница сайта.");
+        Logger.getLogger(webPageController.class.getSimpleName()).info("get controller "+webPageController.class.getSimpleName());
         return "index";
     }
 }
